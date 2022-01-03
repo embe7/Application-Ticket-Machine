dfb349dbf127c26931150041caaffd9673f93b3e
ACER <hendry45xtc@gmail.com>
2022-01-04 00:41:25 +0700

    showJenisTransportasi & viewShowJenisTransportasi

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static String asal;
     public static String tujuan;
+    public static String JenisTransportasi;
 
     public static void main(String[] args) {
-
+        
     }
     //ShowAsalDanTujuan
     public static void showAsalDanTujuan(){
@@ -17,8 +18,17 @@ public class Main {
 //        showAsalDanTujuan();
 //    }
 
+    //ShowJenisTransportasi
+    public static void showJenisTransportasi(){
+        System.out.println("==== Pilih Jenis Transportasi ====");
+        System.out.println("1. Pesawat");
+        System.out.println("2. Kreta");
+        System.out.println("3. Buss");
+    }
+
     //viewShowAsalDanTujuan
     public static void viewShowAsalDanTujuan(){
+        showAsalDanTujuan();
         Scanner InputUser = new Scanner(System.in);
         System.out.print("Asal   : ");
         asal = InputUser.next();
@@ -29,4 +39,13 @@ public class Main {
 //    public static void testViewShowAsalDanTujuan(){
 //        viewShowAsalDanTujuan();
 //    }
+
+    //viewShowJenisTransportasi
+    public static void viewShowJenisTransportasi(){
+        Scanner InputUser = new Scanner(System.in);
+
+        showJenisTransportasi();
+        System.out.print("Pilih Jenis Transportasi : ");
+        JenisTransportasi = InputUser.next();
+    }
 }
